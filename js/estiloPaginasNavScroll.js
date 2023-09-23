574b7b74f3c6efcd60a1b86ee88405cec371d693
felipe-ferr <felipedealbuquerque54@gmail.com>
2023-09-23 01:25:33 -0300

    responsividade

--- js/estiloPaginasNavScroll.js
+++ js/estiloPaginasNavScroll.js
@@ -3,17 +3,14 @@ var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
 
- 
-    if (prevScrollpos > currentScrollPos) {
-        document.getElementById("nav").style.top = "0";
-      } else {
-        document.getElementById("nav").style.top = "-45";
-      }
-      prevScrollpos = currentScrollPos;
-    }
-    
+  if (window.innerWidth > 900){
+     if (prevScrollpos > currentScrollPos) {
+    document.getElementById("nav").style.top = "0";
+  } else {
+    document.getElementById("nav").style.top = "-45";
+  }
+  prevScrollpos = currentScrollPos;
+}
 
-
-
- 
+  }
  
--- js/showNavbarHidden.js
+++ js/showNavbarHidden.js
@@ -1,13 +1,14 @@
-function showNavbarHidden(){
 
-    var navbarhidden = document.getElementById("navhidden");
-
-    if(navbarhidden.style.display = "none"){
-        navbarhidden.style.display = "flex";
-    }
-    else{
-        navbarhidden.style.display = "none";
-    }
+function showSidebar(){
+    document.getElementById("side").style.left = "0";
 
 }
+
+
+function closeSidebar(){
+    document.getElementById("side").style.left = "-400";
+}
+
+
+ 
 
