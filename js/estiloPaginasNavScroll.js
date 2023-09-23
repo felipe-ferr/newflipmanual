74dedc96e6b1269a1bec99c39bbd6207a9164610
felipe-ferr <felipedealbuquerque54@gmail.com>
2023-09-22 23:56:32 -0300

    navbar responsivo (quase)

--- js/estiloPaginasNavScroll.js
+++ js/estiloPaginasNavScroll.js
@@ -1,10 +1,16 @@
 var prevScrollpos = window.pageYOffset;
+
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("nav").style.top = "0";
-  } else {
-    document.getElementById("nav").style.top = "-45px";
+
+  if (window.innerWidth > 900){
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("nav").style.top = "0";
+      } else {
+        document.getElementById("nav").style.top = "-45";
+      }
+      prevScrollpos = currentScrollPos;
+    }
+    
   }
-  prevScrollpos = currentScrollPos;
-}
+ 
---
+++ js/showNavbarHidden.js
@@ -0,0 +1,13 @@
+function showNavbarHidden(){
+
+    var navbarhidden = document.getElementById("navhidden");
+
+    if(navbarhidden.style.display = "none"){
+        navbarhidden.style.display = "flex";
+    }
+    else{
+        navbarhidden.style.display = "none";
+    }
+
+}
+
